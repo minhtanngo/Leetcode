1cdf945a8ef0dd3bc6251e0e8b106fe6b93795db
minhtanngo <74533550+minhtanngo@users.noreply.github.com>
2020-12-08 17:01:57 +0700

    Update reverse-string.py

--- reverse-string.py
+++ reverse-string.py
@@ -1,5 +1,9 @@
 #Link leetcode: https://leetcode.com/problems/reverse-string/
 
+#Write a function that reverses a string. The input string is given as an array of characters char[].
+#Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
