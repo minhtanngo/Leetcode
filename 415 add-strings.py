8288f20fcbce84ef6643341ee9b3dee38ecc128f
minhtanngo <74533550+minhtanngo@users.noreply.github.com>
2020-12-08 17:45:13 +0700

    Update 415 add-strings.py

--- 415 add-strings.py
+++ 415 add-strings.py
@@ -1,3 +1,10 @@
+#Leetcode link: https://leetcode.com/problems/add-strings/
+#Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+#The length of both num1 and num2 is < 5100.
+#Both num1 and num2 contains only digits 0-9.
+#Both num1 and num2 does not contain any leading zero.
+#You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         l = ''
